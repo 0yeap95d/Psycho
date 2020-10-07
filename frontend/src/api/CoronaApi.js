84e0bbc679d39f7656f94f64c2cf0e398adf0e05
한정훈 <onooff@naver.com>
2020-10-08 01:53:28 +0900

    UPDATE | API URL
    Former-commit-id: 3deb3a78a3b8b9c6e19b9de51dfa1702ca90697b

--- frontend/src/api/CoronaApi.js
+++ frontend/src/api/CoronaApi.js
@@ -1,20 +1,20 @@
 import Axios from "axios"
 
-// const apiUrl = "http://localhost:8000/api/covid19data"
-const apiUrl = "https://j3d203.p.ssafy.io:8000/api/covid19data"
+// const apiUrl = "http://localhost:8000/api/"
+const apiUrl = "https://j3d203.p.ssafy.io/api"
 
 const requestCorona = (data, callback, errorCallback) => {
-  Axios.post(apiUrl + "/Covid19Info", data)
+  Axios.post(apiUrl + "/covid19data/Covid19Info", data)
     .then((res) => callback(res))
     .catch((error) => errorCallback(error))
 }
 const requestCoronaGenAge = (data, callback, errorCallback) => {
-  Axios.post(apiUrl + "/Covid19GenAgeInfo", data)
+  Axios.post(apiUrl + "/covid19data/Covid19GenAgeInfo", data)
     .then((res) => callback(res))
     .catch((error) => errorCallback(error))
 }
 const requestCoronaSido = (data, callback, errorCallback) => {
-  Axios.post(apiUrl + "/Covid19SidoInfo", data)
+  Axios.post(apiUrl + "/covid19data/Covid19SidoInfo", data)
     .then((res) => callback(res))
     .catch((error) => errorCallback(error))
 }
