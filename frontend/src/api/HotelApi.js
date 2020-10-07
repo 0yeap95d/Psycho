7ffe9c71cc71df4b825b9bd2d3c9b0e5ee16789a
tjdwob <tjdwob@gmail.com>
2020-10-07 22:06:27 +0900

    UPDATE | API #3
    Former-commit-id: dea9d2923f0858d5765c42c4e180797886ebfaed

--- frontend/src/api/HotelApi.js
+++ frontend/src/api/HotelApi.js
@@ -3,13 +3,13 @@ import Axios from "axios";
 const apiUrl = "https://j3d2203.p.ssafy.io:8000/api";
 
 const requestHotel = (callback, errorCallback) => {
-    Axios.get(apiUrl + 'stores/hotels')
+    Axios.get(apiUrl + '/stores/hotels')
         .then(res => callback(res))
         .catch(error => errorCallback(error))
 }
 
 const requestRecHotel = (data, callback, errorCallback) => {
-    Axios.post(apiUrl + 'stores/recommend/hotels', data)
+    Axios.post(apiUrl + '/stores/recommend/hotels', data)
         .then(res => callback(res))
         .catch(error => errorCallback(error)) 
 }
